2ea9d93b2341423468ffe9a5a332bfdfc17e0dd0
JaredPerez90 <jaredperez@alum.calarts.edu>
2024-12-04 17:40:16 -0800

    uploaded files

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let randomWalker = [];
+let maxWalkers;
+let colorPalette = ['green','blue','orange','pink','red']
+function setup() {
+  createCanvas(400, 400);
+    background('black');
+  
+  maxWalkers = 5
+  
+  for (i = 0; i < maxWalkers; i++)
+    {    randomWalker.push(new                       walker(random(0,width),random(0,height),random(-5,10),colorPalette[i]))
+    }
+  
+
+  
+}
+
+function draw() {
+  
+  for(i = 0; i < maxWalkers; i++)
+{
+  randomWalker[i].display();
+  randomWalker[i].walkingDirection();
+  randomWalker[i].keepInCanvas();
+}
+  
+  
+}
+
